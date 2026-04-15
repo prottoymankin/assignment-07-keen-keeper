dd674b1736fcea05de0304c5704317237ccb2d89
Prottoy Mankin <prottoy032020@gmail.com>
2026-04-15 10:59:11 +0600

    Add toast notification

--- src/components/FriendDetailsPage/CheckInButton.jsx
+++ src/components/FriendDetailsPage/CheckInButton.jsx
@@ -1,12 +1,16 @@
 import { useContext } from "react";
 import { TimelineContext } from "../../context/TimelineContext";
+import { toast } from "react-toastify";
 
 const CheckInButton = ( { title, icon, selectedFriend } ) => {
   const { handleTimeline } = useContext(TimelineContext);
   
   return (
     <button
-      onClick={() => handleTimeline(selectedFriend, title)} 
+      onClick={() => {
+        handleTimeline(selectedFriend, title);
+        toast.success(`${title} with ${selectedFriend.name}`);
+      }} 
       className="cursor-pointer duration-200 flex flex-col items-center gap-2 p-4 rounded-lg shadow-sm text-lg text-[#1F2937] transition active:scale-95"
     >
       <span className="text-3xl">{icon}</span>
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,16 @@ import "./index.css";
 import { RouterProvider } from "react-router/dom";
 import { router } from "./routes/router.jsx";
 import { TimelineContextProvider } from "./context/TimelineContext.jsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <TimelineContextProvider>
     <RouterProvider router={router} />
+
+    <ToastContainer 
+      position="top-center"
+      autoClose={2000}
+      theme="dark"
+    />
   </TimelineContextProvider>
 );
